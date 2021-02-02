c2ee505a6be729eefaa3566bd9cb67bb05a60453
EnzoAlexsander <enzoalexsander9@gmail.com>
2021-02-01 21:58:34 -0300

    [UPDATE] Atualizando os componentes

--- src/components/QuizContainer/index.js
+++ src/components/QuizContainer/index.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 
 const QuizContainer = styled.div`
     width: 100%;
-    max-width: 350px;
-    padding-top: 45px;
+    max-width: 300px;
+    padding-top: 30px;
     margin: auto 10%;
     @media screen and (max-width: 500px) {
         margin: auto;
